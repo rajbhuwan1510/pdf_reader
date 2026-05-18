90dee67e5931b0d45380078c598b61854866789b
Rajbhuwan Jaitawat <92216824+UNG1FT3D@users.noreply.github.com>
2026-05-18 08:50:57 +0530

    chore: remove legacy Node.js/React codebase

--- test-pdf.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const pdf = require('pdf-parse');
-
-let dataBuffer = fs.readFileSync('uploads/somefile'); // I'll need a real file
-
-pdf(dataBuffer).then(function(data) {
-    console.log(data.text);
-});
